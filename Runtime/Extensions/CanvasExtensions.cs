b4af80c5ebb97aea802f2d04a29e05f761b923b1
Adam Graham <adam@zigurous.com>
2021-07-16 23:51:21 -0500

    Add extension methods for UI canvas

---
+++ Runtime/Extensions/CanvasExtensions.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Zigurous.UI
+{
+    /// <summary>
+    /// Extension methods for UI canvas.
+    /// </summary>
+    public static class CanvasExtensions
+    {
+        /// <summary>
+        /// Fades the alpha of all graphics of the canvas to 100% over the given
+        /// <paramref name="duration"/>.
+        /// </summary>
+        /// <param name="canvas">The canvas to fade.</param>
+        /// <param name="duration">The amount of seconds it takes to fade the graphics.</param>
+        /// <param name="ignoreTimeScale">Ignores the time scale when fading the graphics.</param>
+        public static void FadeInGraphics(this Canvas canvas, float duration, bool ignoreTimeScale = false)
+        {
+            Graphic[] graphics = canvas.GetComponentsInChildren<Graphic>();
+
+            for (int i = 0; i < graphics.Length; i++) {
+                graphics[i].CrossFadeAlpha(1.0f, duration, ignoreTimeScale);
+            }
+        }
+
+        /// <summary>
+        /// Fades the alpha of all graphics of the canvas to 0% over the given
+        /// <paramref name="duration"/>.
+        /// </summary>
+        /// <param name="canvas">The canvas to fade.</param>
+        /// <param name="duration">The amount of seconds it takes to fade the graphics.</param>
+        /// <param name="ignoreTimeScale">Ignores the time scale when fading the graphics.</param>
+        public static void FadeOutGraphics(this Canvas canvas, float duration, bool ignoreTimeScale = false)
+        {
+            Graphic[] graphics = canvas.GetComponentsInChildren<Graphic>();
+
+            for (int i = 0; i < graphics.Length; i++) {
+                graphics[i].CrossFadeAlpha(0.0f, duration, ignoreTimeScale);
+            }
+        }
+
+    }
+
+}
